f774454c149c027476243df122623cc3291b283a
sishenhei7 <982505136@qq.com>
2022-06-18 13:07:35 +0800

    fsdfs: test

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+  overrides: [{
+    files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+    env: {
+      jest: true,
+    },
+  }, ],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 110,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all', // 尾随逗号
+  bracketSpacing: true, // 对象中打印空格
+  proseWrap: 'never', // 不允许折行
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false, // 在jsx中把'>' 放同一行
+  insertPragma: false, // 插入@format标识
+  ignorePath: '.prettierignore',
+  filePath: 'src/**/*',
+}
