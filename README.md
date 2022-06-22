f1bf1bcccda6b7ed163d41916849faf52fb833ac
sishenhei7 <982505136@qq.com>
2022-06-22 12:56:59 +0800

    fix: fix build bugs

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # leetcode-crawler
 
 crawl, audit and show people's leetcode data
+
+## preview
+
+https://sishenhei7.github.io/leetcode-crawler/app/index.html
+
+https://sishenhei7.github.io/leetcode-crawler/users/browsnet.json
+
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/leetcode-crawler/app/',
   build: {
     target: 'modules',
-    outDir: 'app'
+    outDir: 'app',
   },
-  plugins: [vue()]
+  plugins: [vue()],
 })
